1611396d0e1f8225d9b5f11ac00509ff541cfc0d
ACER <mrosyadi23@outlook.com>
2020-03-18 21:33:29 +0800

    Hello Git & Java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
   public static void main (String [] args){
-   System.out.pritln("Hello !!");
+   System.out.println("Hello Git !!");
+   System.out.println("Hello Java !!");
    }
 }
